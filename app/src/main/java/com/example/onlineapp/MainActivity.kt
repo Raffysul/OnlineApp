bbd72289b65af6d66a1959374dea7fb6bc771bf4
Rafiat <raffysul1@gmail.com>
2021-05-24 00:09:20 +0100

    Initial commit

---
+++ app/src/main/java/com/example/onlineapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.onlineapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
